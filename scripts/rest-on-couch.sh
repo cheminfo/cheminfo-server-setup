798ab1fe19d0bb0772c6fb76d7e916c0b846e726
Michaël Zasso <mic.besace@gmail.com>
2016-01-07 16:44:43 +0100

    start of rest-on-couch install script

---
+++ scripts/rest-on-couch.sh
@@ -0,0 +1,28 @@
+
+#########################################
+## INSTALL AND CONFIGURE REST-ON-COUCH ##
+#########################################
+
+installRestOnCouch() {
+  mkdir -p /usr/local/rest-on-couch
+  chown nodejs /usr/local/rest-on-couch
+
+  ## Build the config file
+  ROC_CONFIG="{\"homeDir\": \"/usr/local/rest-on-couch\""
+  if
+    [ -n "$COUCHDB_ADMIN_USERNAME" && -n "$COUCHDB_ADMIN_PASSWORD" ]
+  then
+    ROC_CONFIG=${ROC_CONFIG}",\"username\":\"${COUCHDB_ADMIN_USERNAME}\",\"password\":\"${COUCHDB_ADMIN_PASSWORD}\""
+  fi
+  ROC_CONFIG=${ROC_CONFIG}"}"
+  export ROC_CONFIG
+
+  ## Commands that need to be run by the nodejs user
+  su -m nodejs
+  npm install -g rest-on-couch > /dev/null
+  echo ${ROC_CONFIG} > ~/.rest-on-couch-config
+  # TODO clone demo repositories
+  exit
+}
+
+installRestOnCouch
