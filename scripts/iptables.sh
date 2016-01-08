9d51e40c15a1ede23311c52470718bb340121e5a
lpatiny <lpatiny@users.noreply.github.com>
2016-01-08 09:13:47 +0100

    Update iptables.sh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -8,16 +8,22 @@ then
     ok
     info "The service was already configured and CHEMINFO is in the file"
 else
+    if
+        [ ! -z "$SSH_ACCESS" ]
+    then
+        DATA="\\n";
+        for IP in $SSH_ACCESS; do
+            DATA=$DATA"-A INPUT -s $IP -p tcp -m tcp --dport 22 -j ACCEPT\\n"
+        done
+    else
+        DATA="-A INPUT -p tcp --dport 22 -j ACCEPT\\n"
+    fi
+    IPTABLES=`cat ../configs/iptables | sed ':a;N;$!ba;s/\n/\\\\n/g'`
+    DATA="$DATA\\n$IPTABLES\\n"
 
-    for IP in $SSH_ACCESS; do
-
-    done
-
-
-    DATA=`cat ../configs/iptables | sed ':a;N;$!ba;s/\n/\\\\n/g'`
 
     if
-        sed -ibcp "/--dport 22/c$DATA" /etc/sysconfig/iptables
+        sed -i.bcp "/--dport 22/c$DATA" /etc/sysconfig/iptables
     then
         ok
         info "Rules where added in iptables"
