adefb7aaeae1a6d4f3aae5da44e8fa2b94426ec9
Michaël Zasso <mic.besace@gmail.com>
2016-01-08 10:05:41 +0100

    Update rest-on-couch.sh

--- scripts/rest-on-couch.sh
+++ scripts/rest-on-couch.sh
@@ -4,17 +4,21 @@
 #########################################
 
 installRestOnCouch() {
+  
+  message "installing rest-on-couch"
+  
   if 
     [ ! -d "/usr/local/rest-on-couch" ]
   then
     mkdir -p /usr/local/rest-on-couch
-    chown nodejs /usr/local/rest-on-couch
+    chown -r nodejs /usr/local/rest-on-couch
   fi
 
   if 
     [ ! -f "/usr/local/node/.rest-on-couch-config" ]
   then
     ## Build the config file
+    message "building the config file"
     ROC_CONFIG="{\"homeDir\": \"/usr/local/rest-on-couch\""
     if
       [ -n "$COUCHDB_ADMIN_USERNAME" ] && [ -n "$COUCHDB_ADMIN_PASSWORD" ]
@@ -24,20 +28,18 @@ installRestOnCouch() {
     ROC_CONFIG=${ROC_CONFIG}"}"
     export ROC_CONFIG
   
-    ## Commands that need to be run by the nodejs user
-    su nodejs
-    npm install -g rest-on-couch > /dev/null
-    echo ${ROC_CONFIG} > /usr/local/node/.rest-on-couch-config
-    # TODO clone demo repositories
-    exit
+    su nodejs -c 'npm install -g rest-on-couch > /dev/null'
+    su nodejs -c 'echo ${ROC_CONFIG} > /usr/local/node/.rest-on-couch-config'
     
     if
       crontab -l -u nodejs | grep 'rest-on-couch'
     then
+      message "creating crontab entry for import"
       crontab -u nodejs > /tmp/crontab.nodejs
       echo '* * * * *  rest-on-couch import' >> /tmp/crontab.nodejs
       crontab -u nodejs /tmp/crontab.nodejs
       rm -f /tmp/crontab.nodejs
+      ok
     fi
   fi
 }
