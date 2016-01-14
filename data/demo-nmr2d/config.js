572e399eba672529342e9c1264dd46faf6a2b42a
Daniel Kostro <kostro.d@gmail.com>
2016-01-14 09:39:45 +0100

    Update design doc version

--- data/demo-nmr2d/config.js
+++ data/demo-nmr2d/config.js
@@ -10,7 +10,7 @@ module.exports = {
         };
     },
     customDesign: {
-        version: 2,
+        version: 3,
         views: {
             entryByMW: require('../views').entryByMW
         },
